05d50a56b382d728397850a55bfe7947633d884e
magentix <mvion@magentix.fr>
2021-10-11 09:36:23 +0200

    Simplification and reduction of methods

--- Log.php
+++ Log.php
@@ -7,25 +7,51 @@ final class Log
     public const LOG_FILE = 'app.log';
     public const LOG_DIR = __DIR__ . '/../../var/log/';
 
-    public const EMERG  = 0;  // Emergency: system is unusable
-    public const ALERT  = 1;  // Alert: action must be taken immediately
-    public const CRIT   = 2;  // Critical: critical conditions
-    public const ERR    = 3;  // Error: error conditions
-    public const WARN   = 4;  // Warning: warning conditions
-    public const NOTICE = 5;  // Notice: normal but significant condition
-    public const INFO   = 6;  // Informational: informational messages
-    public const DEBUG  = 7;  // Debug: debug messages
+    public const EMERG  = 0; // Emergency: system is unusable
+    public const ALERT  = 1; // Alert: action must be taken immediately
+    public const CRIT   = 2; // Critical: critical conditions
+    public const ERR    = 3; // Error: error conditions
+    public const WARN   = 4; // Warning: warning conditions
+    public const NOTICE = 5; // Notice: normal but significant condition
+    public const INFO   = 6; // Informational: informational messages
+    public const DEBUG  = 7; // Debug: debug messages
 
     /**
      * Log data
      *
-     * @param mixed       $message
-     * @param int         $level
-     * @param string|null $logFile
+     * @param mixed  $message
+     * @param int    $level
+     * @param string $logFile
      *
      * @return int
      */
-    public static function add($message, int $level = self::INFO, ?string $logFile = null): int
+    public static function add($message, int $level = self::INFO, string $logFile = self::LOG_FILE): int
+    {
+        return self::write(self::parseMessage($message, $level), self::LOG_DIR . $logFile);
+    }
+
+    /**
+     * Write log
+     *
+     * @param string $message
+     * @param string $path
+     *
+     * @return int
+     */
+    protected static function write(string $message, string $path): int
+    {
+        return (int) file_put_contents($path, $message, FILE_APPEND);
+    }
+
+    /**
+     * Parse the data to log
+     *
+     * @param mixed $message
+     * @param int   $level
+     *
+     * @return string
+     */
+    protected static function parseMessage($message, int $level): string
     {
         if ($message instanceof Throwable) {
             $message = $message->getMessage() . "\n" . $message->getTraceAsString();
@@ -34,13 +60,8 @@ final class Log
             $message = print_r($message, true);
         }
         $message = addcslashes((string) $message, '<?');
-        $message = self::getPrefix($level) . $message . "\n";
 
-        if (!$logFile) {
-            $logFile = self::LOG_FILE;
-        }
-
-        return (int) file_put_contents(self::LOG_DIR . $logFile, $message, FILE_APPEND);
+        return self::getPrefix($level) . $message . "\n";
     }
 
     /**
